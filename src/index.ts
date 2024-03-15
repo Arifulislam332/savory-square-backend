aeadd18d43954b1bab196afbe53489efca716514
Arifulislam332 <ayatullaharif332@gmail.com>
2024-03-15 12:45:23 +0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+// db connection
+
+
+// express
+const app = express();
+
+// middlewers
+app.use(express.json()); //body parcer
+app.use(cors());
+
+// test api
+app.get("/test", async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Welcome to our server🥰!" });
+});
+
+// listen app
+app.listen(8080, () => {
+  console.log("Server is running on port:8080");
+});
