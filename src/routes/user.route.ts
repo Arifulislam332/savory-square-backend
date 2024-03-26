2f05383b89a4dc7f86b21eda646cdec493371105
Arifulislam332 <ayatullaharif332@gmail.com>
2024-03-26 15:32:41 +0600

    check jwt

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,9 @@
 import express, { Router } from "express";
 import { createCurruser } from "../controllers/user.controller";
+import { jwtCheck } from "../middlewares/auth.middleware";
 
 const router: Router = express.Router();
 
-router.post("/", createCurruser);
+router.post("/", jwtCheck, createCurruser);
 
 export default router;
