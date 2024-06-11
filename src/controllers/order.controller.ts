c0fc83362aaa490d8348f0226092b075bd0c130d
Arifulislam332 <ayatullaharif332@gmail.com>
2024-06-11 18:51:50 +0600

    stripe checkout session

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -35,6 +35,19 @@ export const createCheckoutSession = async (req: Request, res: Response) => {
     }
 
     const lineItems = createLineItems(checkoutSessionReq, restaurant.menuItems);
+
+    const session = await createSession(
+      lineItems,
+      "TEST_ORDER_ID",
+      restaurant.deliveryPrice,
+      restaurant.id.toString()
+    );
+
+    if (!session.url) {
+      return res.status(500).json({ message: "Error creating stripe session" });
+    }
+
+    res.status(200).json({ url: session.url });
   } catch (error: any) {
     console.log(error);
     res.status(500).json({ message: error.raw.message });
@@ -54,6 +67,50 @@ function createLineItems(
       throw new Error("Menu item not found");
     }
 
-    
+    const line_item: Stripe.Checkout.SessionCreateParams.LineItem = {
+      price_data: {
+        currency: "USD",
+        unit_amount: menuItem.price,
+        product_data: {
+          name: menuItem.name,
+        },
+      },
+      quantity: parseInt(cartItem.quantity),
+    };
+
+    return line_item;
   });
+
+  return lineItems;
+}
+
+async function createSession(
+  lineItems: Stripe.Checkout.SessionCreateParams.LineItem[],
+  orderId: string,
+  deliveryPrice: number,
+  restaurantId: string
+) {
+  const sessionData = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          display_name: "Regular Delivery",
+          fixed_amount: {
+            amount: deliveryPrice,
+            currency: "USD",
+          },
+        },
+      },
+    ],
+    mode: "payment",
+    metadata: {
+      orderId,
+      restaurantId,
+    },
+    success_url: `${frontend_url}/order-status?success=true`,
+    cancel_url: `${frontend_url}/detail/${restaurantId}?cancelled=true`,
+  });
+
+  return sessionData;
 }
